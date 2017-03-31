f506bfd57baca75d841893e31fa7e72cd6c8d517
Kenner-G <kenner.galdamez@gmail.com>
2017-03-31 10:10:21 -0600

    Update PocketMine.php

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -73,10 +73,10 @@ namespace pocketmine {
 	use pocketmine\wizard\Installer;
 
 	const VERSION = "1.0";
-	const API_VERSION = "3.0.0-ALPHA1";
-	const CODENAME = "Birpen";
-	const MINECRAFT_VERSION = "v1.0.0 alpha";
-	const MINECRAFT_VERSION_NETWORK = "1.0.0";
+	const API_VERSION = "2.0.0-ALPHA1";
+	const CODENAME = "BLH - Elywing";
+	const MINECRAFT_VERSION = "v1.0.5 alpha";
+	const MINECRAFT_VERSION_NETWORK = "1.0.5";
 	const GENISYS_API_VERSION = '1.9.3';
 
 	/*
